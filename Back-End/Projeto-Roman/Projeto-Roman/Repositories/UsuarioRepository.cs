d60e98676ca17f8dfda6f909abf92f09f45141aa
Leandro Cavalcanti <73174539+LeandroMCF@users.noreply.github.com>
2021-06-23 11:03:09 -0300

    Iniciando API

---
+++ Back-End/Projeto-Roman/Projeto-Roman/Repositories/UsuarioRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Projeto_Roman.Repositories
+{
+    public class UsuarioRepository
+    {
+    }
+}
